5360f00ef669aa423366b43471f25f8363f988d4
Stas <st520vip20@gmail.com>
2020-08-22 20:12:36 +0300

    220820201

---
+++ Lessons 2/HomeWork/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число");
+            Double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите 2 число");
+            Double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сумма чисел:{0} \t Разница чисел:{1} \t Произведение чисел:{2}", (firstNumber + secondNumber), (firstNumber - secondNumber), (firstNumber * secondNumber));
+
+        }
+    }
+}

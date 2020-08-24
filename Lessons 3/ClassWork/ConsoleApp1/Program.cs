4d5110dbca4db563657eba30225241e7b6ab1cf1
valen2902 <valya.bouzah@yandex.ru>
2020-08-24 21:34:04 +0300

    24082020

---
+++ Lessons 3/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			object a = 5;
+			int b = (int) a + 5;  //приведение типов
+
+			Console.WriteLine(b);
+			*/
+			string[] mas = new string [5];
+				for (int i = 0; i < mas.Length; i++)
+				{
+				mas[i] = Console.ReadLine();
+				}
+
+			for (int i = 0; i < mas.Length; i++)
+			{
+				Console.WriteLine(mas[i]);
+			}
+
+
+
+		}
+	}
+}

03e533d88335cb5a206279e9d3555a72dbc4471c
Stas <st520vip20@gmail.com>
2020-08-25 19:41:33 +0300

    25.08.2020

---
+++ Lessons 3/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] names = new string[3];
+            int [] age = new int[3];
+
+        
+            for (int i = 0; i< names.Length; i++)
+			{
+
+            Console.WriteLine("Введите имя человека:");
+                names[i] = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст человек:");
+                age[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("--------------------------");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Имя: "+names[i]+ ",\t возраст через 4 года: " + "\t"+ + (age[i]+4));
+            }
+            Console.ReadKey();
+        }
+    }
+}

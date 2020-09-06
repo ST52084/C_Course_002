e85d55bfa670778ffd98200c4e46cac1739ef045
Stas <st520vip20@gmail.com>
2020-09-06 19:36:11 +0300

    20.09.06_02

--- Lessons 6/HomeWork/ConsoleApp2/Program.cs
+++ Lessons 6/HomeWork/ConsoleApp2/Program.cs
@@ -6,7 +6,49 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal firstVznos = ReadNumberWithoutException("Введите сумму первоначального взноса в рублях:");
+            decimal everyDayPay = ReadNumberWithoutException("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0,01)");
+            decimal sumRequired = ReadNumberWithoutException("Введите желаемую сумму накопления в рублях:");
+
+
+            if (firstVznos == 0)
+                Console.WriteLine("Так ничего не получится");
+            else if (everyDayPay == 0)
+                Console.WriteLine("Так ничего не получится");
+            else if (sumRequired == 0)
+                Console.WriteLine("Так ничего не получится");
+            else
+            {
+
+
+                int daysRequired = 0;  /// Число требуемых дней до накопления
+
+                do
+                {
+                    firstVznos = firstVznos + firstVznos * everyDayPay;
+                    daysRequired++;
+                } while (firstVznos < sumRequired);
+
+
+                Console.WriteLine("Необходимое количество дней для накопления желаемой суммы: " + daysRequired);
+            }
+        }
+
+        static decimal ReadNumberWithoutException(string text)
+        {
+            Console.WriteLine(text);
+            decimal number = 0;
+            try
+            {
+                var vvod = Console.ReadLine().Replace('.', ',');
+                number = Convert.ToDecimal(vvod);
+                return number;
+            }
+
+            catch (System.FormatException)
+            {
+                return 0;
+            }
         }
     }
 }

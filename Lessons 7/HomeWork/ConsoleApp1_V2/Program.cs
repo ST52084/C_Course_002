d2dec8ff1ff50c18f16d72f9c7b83004659de6e5
Stas <st520vip20@gmail.com>
2020-09-13 12:04:25 +0300

    V2

---
+++ Lessons 7/HomeWork/ConsoleApp1_V2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayWord = new string[] { };
+            int cWord = 0;
+            Console.WriteLine("Введите строку из нескольких слов:");
+
+            do
+            {
+                arrayWord = ReadWord();
+                foreach (var item in arrayWord)
+                {
+
+                    if (item.LastIndexOf("А") == 0)
+                    {
+                        cWord++;
+                    }
+                }
+
+            } while (arrayWord.Length == 0);
+            Console.WriteLine("Количество слов, начинающихся с буквы 'А': " + cWord);
+        }
+        static string[] ReadWord()
+        {
+            string ReadWord = Console.ReadLine();
+            string[] arrayWord = ReadWord.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);   /// делим строку на массив по заданному символу
+            if (arrayWord.Length >= 2)
+            {
+                return arrayWord;
+            }
+            else
+            {
+                Console.WriteLine("Слишком мало слов: (Попробуйте ещё раз: ");
+                return new string[] { };
+            }
+        }
+    }
+}

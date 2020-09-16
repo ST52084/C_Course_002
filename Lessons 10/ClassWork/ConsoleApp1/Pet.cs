7ca84031a71c6c067e3d4584009fa46d7f8128f3
valen2902 <valya.bouzah@yandex.ru>
2020-09-16 21:44:11 +0300

    200916

---
+++ Lessons 10/ClassWork/ConsoleApp1/Pet.cs
@@ -0,0 +1,81 @@
+﻿namespace ConsoleApp1
+{
+	public class Pet
+	{
+		string kind;
+		public string Kind
+		{
+			set
+			{
+				if (value != "")
+				{
+					kind = value;
+				}
+
+			}
+			get
+			{
+				return kind;
+			}
+		}
+
+		string name;
+		public string Name
+		{
+			set
+			{
+				if (value != "")
+				{
+					name = value;
+				}
+
+			}
+			get
+			{
+				return name;
+			}
+		}
+
+		char sex;
+		public char Sex
+		{
+			set
+			{
+				if (value == 'M' | value == 'F')
+				{
+					sex = value;
+				}
+
+			}
+			get
+			{
+				return sex;
+			}
+		}
+
+		int age;
+		public int Age
+		{
+			set
+			{
+				if (value < 100)
+				{
+					age = value;
+				}
+			}
+			get
+			{
+				return age;
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return $"{name},{kind},{sex},{age}";
+			}
+		}
+
+	}
+}
---
+++ Lessons 10/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var pet = new Pet();
+			var pet2 = new Pet();
+
+			pet.Kind = "Cat";
+			pet.Name = "Tom";
+			pet.Sex = 'M';
+			pet.Age = 2;
+
+			Console.WriteLine(pet.Description);
+			
+
+		}
+	}
+}

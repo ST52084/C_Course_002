91e6a76770b7d17cd743556f6c0ecd8baff12966
Stas <st520vip20@gmail.com>
2020-09-19 00:17:56 +0300

    200919

---
+++ Lessons 10/HomeWork/Class people/People.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Class_people
+{
+    public class People
+    {
+
+        string name;
+        public string Name
+        {
+            set
+            {
+                char[] array = new char[value.Length];
+                for (int i = 0; i < value.Length; i++)
+                {
+                    array[i] = value[i];
+                    if (!char.IsLetter(array[i]) | (string.IsNullOrEmpty(value.ToString()) == true))
+                    {
+                        throw new Exception("В имени должны быть только печатные символы.");
+                    }
+                    else
+                    {
+                        name = value;
+                    }
+                }
+            }
+        }
+
+        private int _ageAfter_4_Years;
+
+        public string Age
+        {
+            set
+            {
+                try
+                {
+                    _ageAfter_4_Years = Convert.ToInt32(value) + 4;
+                }
+                catch (FormatException)
+                {
+
+                    throw new Exception("Возраст должен быть целым числом.");
+                }
+
+            }
+        }
+
+        public int AgeAfter_4_Years
+        {
+            get
+            {
+                return _ageAfter_4_Years;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Name: " + name + ", age in 4 years: " + _ageAfter_4_Years;
+            }
+        }
+    }
+}
---
+++ Lessons 10/HomeWork/Class people/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Class_people
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            
+            People[] people = new People[3];
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = new People();
+                Console.Write($"Enter name {i}:");
+                people[i].Name = Console.ReadLine();
+
+                Console.Write($"Enter age {i}:");
+                people[i].Age = Console.ReadLine();
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine(people[i].Description);
+            }
+            Console.ReadKey();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}

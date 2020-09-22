9a0b8b5b09f1aa05440366ffef7d2dff7e4f02a3
Stas <st520vip20@gmail.com>
2020-09-22 21:18:03 +0300

    200922

---
+++ Lessons 11/HomeWork/Alarm_clock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alarm_clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarm_clock01 = new ReminderItem(DateTimeOffset.Parse("23.09.2020 23:00:00"), "Подъём!");
+            var alarm_clock02 = new ReminderItem(DateTimeOffset.Parse("21.09.2020 23:00:00"), "Подъём!");
+            //Console.WriteLine("Текущее время: \t"+ DateTime.Now);
+            alarm_clock01.WriteProperties();
+            Console.WriteLine("************************************************************************");
+            alarm_clock02.WriteProperties();
+        }
+    }
+}
---
+++ Lessons 11/HomeWork/Alarm_clock/ReminderItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alarm_clock
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate;
+        public string AlarmMessage;
+
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        private TimeSpan _TimeToAlarm
+        {
+            get
+            {
+                return (TimeSpan)(DateTime.Now - AlarmDate);
+            }
+        }
+
+        private bool _IsOutdated
+        {
+            get
+            {
+                if (_TimeToAlarm >= TimeSpan.Parse("0:00:00"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public void WriteProperties()
+        {
+            Console.WriteLine($"Дата и время будильника:  \t {AlarmDate} " +
+                $"\nСообщение будильника:  \t {AlarmMessage} " +
+                $"\nВремя до срабатывания: \t {_TimeToAlarm}" +
+                $"\nПросрочено ли событие: \t {_IsOutdated} ");
+        }
+    }
+}
+

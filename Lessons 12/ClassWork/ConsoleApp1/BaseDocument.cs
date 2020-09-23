de55e160853e8c280767aead603189461c18a394
Anton3topora <k.grosheva.2000@gmail.com>
2020-09-23 21:31:01 +0300

    200923

---
+++ Lessons 12/ClassWork/ConsoleApp1/BaseDocument.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class BaseDocument
+    {
+        public string Title;
+        public string Number;
+        public DateTimeOffset IssueData;
+        public virtual  string Description =>
+          $"Title: {Title}, Number: {Number}, IssueData: {IssueData}";
+
+
+        public BaseDocument(string title, string number, DateTimeOffset iossueData)   
+        {
+            Title = title;
+            Number = number;
+            IssueData = iossueData;
+        }
+
+
+        public void WriteToConsole()
+        {
+            Console.WriteLine(Description);
+        }
+
+    }
+
+    class Passport : BaseDocument
+    {
+        public string Country;
+        public string PersonName;
+
+        public Passport(string country, string persoName, string number, DateTimeOffset iossueData) : base("Pasport", number, iossueData)
+        {
+            
+            Country = country;
+            PersonName = persoName;
+        }
+
+        public override string Description =>
+         $"Title: {Title}, Number: {Number}, IssueData: {IssueData}, Country: {Country}, PersonName: {PersonName}";
+
+        //public new void WriteToConsole()
+        //{
+        //    Console.WriteLine(Description);
+        //}
+
+        public void ChangeissueDate(DateTimeOffset newlssuseDate)
+        {
+            IssueData = DateTime.Now;
+        }
+    }
+}
---
+++ Lessons 12/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var person = new BaseDocument("Pass", "00-01", DateTimeOffset.Parse("01.01.2018"));
+            //var person01 = new Passport("Rus", "Tom", "00-01", DateTimeOffset.Parse("01.01.2018"));
+            //var person = new BaseDocument();
+            //person.Title = "Pass";
+            //person.Number = "00-01";
+            //person.IssueData = DateTimeOffset.Parse("01.01.2018");
+
+            //var person01 = new Passport();
+            //person01.Title = "Pass01";
+            //person01.Number = "00-02";
+            //person01.IssueData = DateTimeOffset.Parse("01.01.2017");
+            //person01.Country = "Rus";
+            //person01.PersonName = "Tom";
+
+            //person.WriteToConsole();
+            //person01.WriteToConsole();
+
+            var Pas = new BaseDocument[]
+            {
+            new BaseDocument("Pass", "00-01", DateTimeOffset.Parse("01.01.2018")),
+            new Passport("Rus", "Tom", "00-01", DateTimeOffset.Parse("01.01.2018")),
+        };
+
+            foreach (var item in Pas)
+            {
+                if (item is Passport passport)
+                {
+                    passport.ChangeissueDate(DateTime.Now);
+                }
+                item.WriteToConsole();
+
+            }
+
+
+        }
+    }
+}

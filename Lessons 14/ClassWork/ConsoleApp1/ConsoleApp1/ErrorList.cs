971d5d3c440f695965277e2728a347fad3090d9f
ilya <ilyakurilev@gmail.com>
2020-09-30 21:40:08 +0300

    200930

---
+++ Lessons 14/ClassWork/ConsoleApp1/ConsoleApp1/ErrorList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class ErrorList : IDisposable, IEnumerable<string>
+	{
+
+		public string Category		
+		{
+			get;
+		}
+
+		private List<string> _Errors
+		{
+			get; set;
+		}
+
+		public void Dispose()
+		{
+			_Errors.Clear();
+			_Errors = null;
+		}
+
+		public ErrorList(string category)  // Конструктор
+		{
+			Category = category;
+			_Errors = new List<string>();
+		}
+
+		public void Add(string errorMessage)
+		{
+			_Errors.Add(errorMessage);
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return ((IEnumerable<string>)_Errors).GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable)_Errors).GetEnumerator();
+		}
+	}
+}

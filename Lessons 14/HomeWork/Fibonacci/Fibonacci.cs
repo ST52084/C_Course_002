3bca1de1767ca89c800de61cc3795fb0fa414e2e
Stas <st520vip20@gmail.com>
2020-10-03 16:26:04 +0300

    201003

---
+++ Lessons 14/HomeWork/Fibonacci/Fibonacci.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    public class Fibonacci : IEnumerator
+    {
+        List<int> _fibonacci_coll = new List<int>();
+        int _position = 0;
+        int _previous = 1;
+        int _current = 0;
+        int _next;
+
+        public object Current
+        {
+            get
+            {
+                _fibonacci_coll.Add(_previous);
+                return _previous;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (_position < 10)
+            {
+                _position++;
+                _next = _current + _previous;
+                _previous = _current;
+                _current = _next;
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            _position = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+    }
+}
--- Lessons 14/HomeWork/Fibonacci/Program.cs
+++ Lessons 14/HomeWork/Fibonacci/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Fibonacci
 {
@@ -6,7 +9,32 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (int item in Fibonacci(10))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Fibonacci fibonacci_coll = new Fibonacci();
+            foreach (var item in fibonacci_coll)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable Fibonacci(int number)    
+        {
+            int previous = 0;
+            int current = 1;
+            int i = 0;
+            while (i < number)
+            {
+                yield return previous;
+                int next = previous + current;
+                previous = current;
+                current = next;
+                i++;
+            }
         }
     }
 }
+

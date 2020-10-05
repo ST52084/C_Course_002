8cff985215c7b74955317de25349ea5d43289225
ilya <ilyakurilev@gmail.com>
2020-10-05 22:09:32 +0300

    02

---
+++ Lessons 15/ClassWork/ConsoleApp1/ConsoleApp1/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class Account<T>
+	{
+		public T Id { get; private set; }
+		public string Name { get; private set; }
+
+		public Account(T id, string name)  // Конструктор
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public void WriteProperties() 
+		{
+			Console.WriteLine($"{Id}, {Name}");
+		}
+	}
+}
---
+++ Lessons 15/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var acc1 = new Account<int>(10, "AS");
+			acc1.WriteProperties();
+
+			var acc2 = new Account<string>("10", "AS");
+			acc2.WriteProperties();
+
+			var acc3 = new Account<Guid>(Guid.NewGuid(), "AS");
+			acc3.WriteProperties();
+			//////////////////////////////
+
+			var a = new Circle(10);
+			var b = a.Calculate(S);
+			Console.WriteLine(b);
+
+			var c = a.Calculate(P);
+			Console.WriteLine(c);
+		}
+
+		public static double S(double r)
+		{
+			return 3.14*r*r;
+		}
+
+		public static double P(double r)
+		{
+			return 2*3.14 * r;
+		}
+
+		// public double Calculate(Func<double, double, operation>);
+		public delegate double CircleDelegate(double a);
+
+		public sealed class Circle
+		{
+			private double _radius;
+			public Circle(double radius)
+			{
+				_radius = radius;
+			}
+
+			public double Calculate(CircleDelegate operation)
+			{
+				return operation(_radius);
+			}
+		}
+	}
+}

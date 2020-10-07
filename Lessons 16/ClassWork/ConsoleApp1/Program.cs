56f437954869ceff6c03a2c5fc393cf4dc2fdb0f
alinasimo <alinasimo1705@mail.ru>
2020-10-07 21:11:33 +0300

    03

--- Lessons 16/ClassWork/ConsoleApp1/Program.cs
+++ Lessons 16/ClassWork/ConsoleApp1/Program.cs
@@ -4,9 +4,37 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var a = new Circle(10);
+            Console.WriteLine(a.Calculate((double b)=> 3.14 * b * b));
+
+            var b = new Circle(10);
+            Console.WriteLine(b.Calculate((double a) => 2 * 3.14 * a));
+
+            var r = new Circle(10);
+            Console.WriteLine(r.Calculate((double a) => 2 * a));
+
+
         }
     }
+
+    class Circle
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Calculate(Func<double, double> so)
+        {
+            return so(_radius);
+        }
+
+    }
+
 }

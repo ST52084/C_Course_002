0b538864f428ad62028d26ba559378c482632f5c
alinasimo <alinasimo1705@mail.ru>
2020-10-12 20:20:50 +0300

    001

---
+++ Lessons 17/ClassWork/ConsoleApp1/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Generator
+    {
+        public event Action<byte[]> DataGenerator; // создаем событие
+        public void Generate(int count)
+        {
+            byte[] a = new byte[count];
+            Random rand = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                a[i] = Convert.ToByte(rand.Next(0, 100));
+            }
+            DataGenerator?.Invoke(a);   // проверяем на нуль
+        }
+        // ctor - горячие клавиши для создания конструктора
+    }
+}
---
+++ Lessons 17/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Generator();
+            a1.DataGenerator += A1_DataGenerator; // подписываемся на событие
+            a1.Generate(10);
+        }
+
+        private static void A1_DataGenerator(byte[] obj)
+        {
+            File.WriteAllBytes("txt.txt", obj);
+        }
+    }
+}

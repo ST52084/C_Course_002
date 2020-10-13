830f31014e8877fc766502fb474953d89627eddf
Stas <st520vip20@gmail.com>
2020-10-13 22:27:20 +0300

    201013

---
+++ Lessons 17/HomeWork/ConsoleApp1/FileWriterWithProgress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class FileWriterWithProgress
+    {
+        public event Action WritingPerformed;
+        public event Action WritingCompleted;
+
+        public void WriteBytes(string fileName, byte[] data, float percentageToFireEvent)
+        {
+            for (int i = 0; i < (Math.Truncate(1 / percentageToFireEvent)); i++)
+            {
+                WritingPerformed += A1_WritingPerformed;
+
+            }
+            WritingCompleted += A1_WritingCompleted;
+
+            WritingPerformed?.Invoke();
+            WritingCompleted?.Invoke();
+        }
+
+        private void A1_WritingCompleted()
+        {
+            Console.WriteLine("Событие - WritingCompleted");
+        }
+
+        private void A1_WritingPerformed()
+        {
+            Console.WriteLine("Событие - WritingPerformed");
+        }
+    }
+
+}
---
+++ Lessons 17/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte[] data = new Byte[4] { 1, 2, 3, 5 };
+
+            var writer = new FileWriterWithProgress();
+            writer.WriteBytes("log.txt", data, Convert.ToSingle(0.15));
+        }
+    }
+}
